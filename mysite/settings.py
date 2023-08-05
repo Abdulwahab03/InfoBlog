4ddeb48ea75f46d4f78dc509338811241d4eea03
Wahab Saheed Oluwatoyin <swahab947@gmail.com>
2023-08-05 15:10:48 +0100

    my initial commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-2b68-p3%@7n8mcr_6nuqqzbza@&x4uf^rbgi6^b)h&f!$2640x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 # Email server configuration
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
@@ -124,6 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
+STATIC_DIRS = [os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
